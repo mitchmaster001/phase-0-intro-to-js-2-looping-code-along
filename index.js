889a29472cd2b36ac595e837095bfb6d51ce2277
Charles Njoroge <charles.maina@student.moringaschool.com>
2023-03-10 19:50:34 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+//code your solutions in this file
+let names=["Mohaa" , "Sly" , "Breezy"];
+let x="birthday";
+
+function writecards(names ,x){
+
+    let Result =[];
+    for (let i = 0; i < names.length; i++) {
+        result.push(names[i] + " " + x);
+    }
+}
+     result.push("Thankyou) ${names[i]},for the wonderful ${x} gift!")
+//debugger
+
+return result;
+
+
+//let returndvalue=writeCards(names, X);
+//console.log(writeCards(["Mohaa" , "Sly" , "Breezy"], "birthday"));
+
+function countDown(num){
+    while (num >0) {
+        console.log(num);
+        
+    }
+
+}
+
+countDown();
